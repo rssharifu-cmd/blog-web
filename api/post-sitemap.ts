2e81e1249b2e2d073984a156872bda906d501acc
rssharifu-cmd <rssharifu@gmail.com>
2026-09-26 13:04:43 +0300

    feat: add XSLT stylesheet for human-readable sitemaps
    Link a new sitemap stylesheet to all sitemap XML files to improve readability and user experience when accessing sitemaps directly in the browser.

--- api/post-sitemap.ts
+++ api/post-sitemap.ts
@@ -81,6 +81,7 @@ export default async function handler(req: any, res: any) {
     }
 
     let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap-style.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
--- api/sitemap-index.ts
+++ api/sitemap-index.ts
@@ -13,6 +13,7 @@ export default async function handler(req: any, res: any) {
     const currentDate = new Date().toISOString().split('T')[0];
 
     const sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap-style.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${baseDomain}/post-sitemap.xml</loc>
